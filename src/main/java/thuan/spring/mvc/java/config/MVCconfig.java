9780cb5ff93d9dcee74ba3cac6926dbd7bdc7363
Thuan Tran <thuan.uit@gmail.com>
2023-04-29 14:21:51 +0700

    remove web.xml

--- src/main/java/thuan/spring/mvc/java/config/MVCconfig.java
+++ src/main/java/thuan/spring/mvc/java/config/MVCconfig.java
@@ -27,6 +27,7 @@ public class MVCconfig implements WebMvcConfigurer {
 
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("index");
 		registry.addViewController("/index2").setViewName("index2");
 		registry.addViewController("/login").setViewName("login");
 	}
